67048b66fbc28995ed7e3f2433ba9c51e724adef
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-06-23 21:08:12 -0300

    Authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,30 @@
 import { FaGithub } from "react-icons/fa";
-import { FiX } from 'react-icons/fi'
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Diego Fernandes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
-  )
+  );
 }
