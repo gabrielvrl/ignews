98dd6edbdb5aa018f4326a2554e9bded3f95a9ba
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-06-03 12:43:25 -0300

    Salvando usuários no Github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
+import { query as q } from 'faunadb'
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,25 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 });
