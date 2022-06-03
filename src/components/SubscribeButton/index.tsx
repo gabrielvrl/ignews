1ef4a495c8d7fdc4302cde4c1c8f50c8d9ac7a4e
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-06-03 16:12:39 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,11 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId}: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn('github')
       return;
     }
+    
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId });
+    } catch(err) {
+      alert(err.message);
+    }
+
   }
 
   return (
