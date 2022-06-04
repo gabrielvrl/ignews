618128c638dcbda3fc500ab02a2a4ea6d6f3b407
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-06-04 14:57:13 -0300

    Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,61 +24,39 @@ export const config = {
 
 const relevantEvents = new Set([
   'checkout.session.completed',
-  'customer.subscription.updated',
-  'customer.subscription.deleted',
 ])
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const buf = await buffer(req);
-
+    const buf = await buffer(req)
     const secret = req.headers['stripe-signature']
 
     let event: Stripe.Event;
 
     try {
-      event = stripe.webhooks.constructEvent(
-        buf,
-        secret,
-        process.env.STRIPE_WEBHOOK_SECRET
-      )
-
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
-      return res.status(400).send(`Webhook error: ${err.message} `)
+      return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
     const { type } = event;
 
-    if (relevantEvents.has(type)) {
+    if(relevantEvents.has(type)) {
       try {
-        switch (type) {
-          case 'customer.subscription.updated':
-          case 'customer.subscription.deleted':
-
-            const subscription = event.data.object as Stripe.Subscription
-
-            await saveSubscription(
-              subscription.id,
-              subscription.customer.toString(),
-              false,
-            );
-
-            break;
-
+        switch(type) {
           case 'checkout.session.completed':
-            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString(),
-              true
+              checkoutSession.customer.toString()
             )
 
             break;
           default:
-            throw new Error('unhandled event.')
-        }
+            throw new Error('Unhandled event.')
+        }  
       } catch (err) {
         return res.json({ error: 'Webhook handler failed.' })
       }
@@ -86,9 +64,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     res.json({ received: true })
   } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method not allowed')
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
-
-
 }
