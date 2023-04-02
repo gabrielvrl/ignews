c7c4bedc1c2987c7d8e6fe4e0d9899f565353af0
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-04-02 19:13:33 -0300

    Testando getStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import Home, { getStaticProps } from '../../pages';
+import { stripe } from '../../services/stripe';
+import { mocked } from 'jest-mock';
 
 jest.mock('next-auth/client', () => {
   return {
@@ -9,6 +11,8 @@ jest.mock('next-auth/client', () => {
 
 jest.mock('next/router');
 
+jest.mock('../../services/stripe');
+
 describe('Home page', () => {
   it('renders correctly', () => {
     render(<Home product={{
@@ -17,5 +21,28 @@ describe('Home page', () => {
     }} />);
 
     expect(screen.getByText('for R$10,00 month')).toBeInTheDocument();
+  });
+
+
+  it('loads initial data', async () => {
+    const retriveStripePricesMocked = mocked(stripe.prices.retrieve)
+
+    retriveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({} as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    );
   });
 });
