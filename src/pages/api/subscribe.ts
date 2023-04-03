89b7ce2a454e0270cc1a9c33d91d38c17d44e42b
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-04-02 22:24:30 -0300

    next auth react

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
+import { getSession } from "next-auth/react";
 import { fauna } from "../../services/fauna";
 import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
